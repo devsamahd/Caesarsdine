cbbb653b51b275750ba8e1d350905782bf68de2d
devsamahd <devsamahd@gmail.com>
2022-09-26 20:34:18 +0100

    just another commit

---
+++ morejs.js
@@ -0,0 +1,24 @@
+const callapi = (charactername) => {
+    //don't forget access token
+    fetch(``)
+    .then(response=> response.json())
+    .then(data => {
+        data.
+    })
+}
+const callapi = (charactername) => {
+    //don't forget access token
+    fetch(``)
+    .then(response=> response.json())
+    .then(data => {
+        data.
+    })
+}
+const callapi = (charactername) => {
+    //don't forget access token
+    fetch(``)
+    .then(response=> response.json())
+    .then(data => {
+        data.
+    })
+}
